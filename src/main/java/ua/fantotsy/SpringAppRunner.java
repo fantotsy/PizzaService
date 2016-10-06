deabb390239da86eef6faf2dcb362fab7beb3c74
Anton_Tsymbal <anton_tsymbal@epam.com>
2016-10-06 14:02:10 +0300

    SpringAppRunner modified.

--- src/main/java/ua/fantotsy/SpringAppRunner.java
+++ src/main/java/ua/fantotsy/SpringAppRunner.java
@@ -2,7 +2,9 @@ package ua.fantotsy;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ua.fantotsy.domain.Order;
 import ua.fantotsy.repository.pizza.PizzaRepository;
+import ua.fantotsy.services.order.OrderService;
 
 import java.util.Arrays;
 
@@ -13,7 +15,9 @@ public class SpringAppRunner {
         System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
         PizzaRepository pizzaRepository = (PizzaRepository) context.getBean("pizzaRepository");
         System.out.println(pizzaRepository.getPizzaById(1));
-
+        OrderService orderService = (OrderService) context.getBean("orderService");
+        Order order = orderService.placeNewOrder(null, 1, 2, 3);
+        System.out.println(order);
         context.close();
     }
 }
