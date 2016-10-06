0f9c403ed30fb70ed85c7a00733b52af22c7e4a5
Anton_Tsymbal <anton_tsymbal@epam.com>
2016-10-06 13:04:32 +0300

    Beans added.

--- src/main/java/ua/fantotsy/SpringAppRunner.java
+++ src/main/java/ua/fantotsy/SpringAppRunner.java
@@ -2,11 +2,17 @@ package ua.fantotsy;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ua.fantotsy.repository.pizza.PizzaRepository;
+
+import java.util.Arrays;
 
 public class SpringAppRunner {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
+        System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
+        PizzaRepository pizzaRepository = (PizzaRepository) context.getBean("pizzaRepository");
+        System.out.println(pizzaRepository.getPizzaById(1));
 
         context.close();
     }
