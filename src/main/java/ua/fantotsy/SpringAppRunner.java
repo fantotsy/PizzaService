581bd1cfedd20906031d9a47ffe94608486a2328
Anton_Tsymbal <anton_tsymbal@epam.com>
2016-10-06 14:53:09 +0300

    Code refactored.

--- src/main/java/ua/fantotsy/SpringAppRunner.java
+++ src/main/java/ua/fantotsy/SpringAppRunner.java
@@ -4,6 +4,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ua.fantotsy.domain.Order;
 import ua.fantotsy.repository.pizza.PizzaRepository;
+import ua.fantotsy.services.SomeService;
 import ua.fantotsy.services.order.OrderService;
 
 import java.util.Arrays;
@@ -11,13 +12,21 @@ import java.util.Arrays;
 public class SpringAppRunner {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml", "appContext_1.xml");
-        System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
-        PizzaRepository pizzaRepository = (PizzaRepository) context.getBean("pizzaRepository");
+        ConfigurableApplicationContext repoContext = new ClassPathXmlApplicationContext("repoContext.xml");
+        System.out.println(Arrays.toString(repoContext.getBeanDefinitionNames()));
+
+        ConfigurableApplicationContext appContext =
+                new ClassPathXmlApplicationContext(new String[]{"appContext.xml"}, repoContext);
+        System.out.println(Arrays.toString(appContext.getBeanDefinitionNames()));
+
+        System.out.println(repoContext.getBean("T1", SomeService.class).getString());
+        System.out.println(appContext.getBean("T1", SomeService.class).getString());
+
+        PizzaRepository pizzaRepository = (PizzaRepository) repoContext.getBean("pizzaRepository");
         System.out.println(pizzaRepository.getPizzaById(1));
-        OrderService orderService = (OrderService) context.getBean("orderService");
+        OrderService orderService = (OrderService) appContext.getBean("orderService");
         Order order = orderService.placeNewOrder(null, 1, 2, 3);
         System.out.println(order);
-        context.close();
+        repoContext.close();
     }
 }
