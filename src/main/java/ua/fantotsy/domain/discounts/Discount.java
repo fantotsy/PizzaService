2d26c267f31adfee237a18b2a2b579d9c612fc1e
fantotsy <fantotsy@gmail.com>
2016-10-12 21:19:55 +0300

    Code refactored.

--- src/main/java/ua/fantotsy/domain/discounts/Discount.java
+++ src/main/java/ua/fantotsy/domain/discounts/Discount.java
@@ -3,15 +3,18 @@ package ua.fantotsy.domain.discounts;
 import ua.fantotsy.domain.Order;
 
 public abstract class Discount {
+    /*Fields*/
     private String name;
     private DiscountState state;
     protected Order order;
 
+    /*Constructors*/
     Discount() {
         name = this.getClass().getSimpleName();
         state = DiscountState.ACTIVE;
     }
 
+    /*Internal Objects*/
     public enum DiscountState {
         ACTIVE {
             @Override
@@ -29,14 +32,17 @@ public abstract class Discount {
         public abstract DiscountState reverseState();
     }
 
+    /*Abstract Methods*/
     public abstract boolean canBeApplied(Order order);
 
     public abstract double getDiscount(Order order);
 
+    /*Public Methods*/
     public void changeState() {
         state = state.reverseState();
     }
 
+    /*Getters & Setters*/
     public String getName() {
         return name;
     }
@@ -48,27 +54,22 @@ public abstract class Discount {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof Discount)) return false;
 
         Discount discount = (Discount) o;
 
-        if (name != null ? !name.equals(discount.name) : discount.name != null) return false;
-        return state == discount.state;
+        return name != null ? name.equals(discount.name) : discount.name == null;
 
     }
 
     @Override
     public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (state != null ? state.hashCode() : 0);
-        return result;
+        return name != null ? name.hashCode() : 0;
     }
 
     @Override
     public String toString() {
-        return "Discount{" +
-                "name='" + name + '\'' +
-                ", state=" + state +
-                '}';
+        return "Discount name: " + name +
+                "\nDiscount state: " + state.name() + "\n";
     }
 }
