67db22d055356a9976141255c1d9ec21c36cb73e
fantotsy <fantotsy@gmail.com>
2016-10-16 16:41:54 +0300

    SimpleAccumulativeCardServiceTest done.

---
+++ src/test/java/ua/fantotsy/services/accumulativeCard/SimpleAccumulativeCardTest.java
@@ -0,0 +1,32 @@
+package ua.fantotsy.services.accumulativeCard;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import ua.fantotsy.domain.AccumulativeCard;
+import ua.fantotsy.repository.accumulativeCard.AccumulativeCardRepository;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleAccumulativeCardTest {
+    SimpleAccumulativeCardService accumulativeCardService;
+    @Mock
+    AccumulativeCardRepository accumulativeCardRepositoryMock;
+    @Mock
+    AccumulativeCard accumulativeCardMock;
+
+    @Before
+    public void setUp() {
+        accumulativeCardService = spy(new SimpleAccumulativeCardService(accumulativeCardRepositoryMock));
+        doReturn(accumulativeCardMock).when(accumulativeCardService).createNewAccumulativeCard();
+    }
+
+    @Test
+    public void testAddAndReturnNewAccumulativeCardAddsCard() {
+        accumulativeCardService.addAndReturnNewAccumulativeCard();
+        verify(accumulativeCardRepositoryMock).addNewAccumulativeCard(accumulativeCardMock);
+    }
+}
