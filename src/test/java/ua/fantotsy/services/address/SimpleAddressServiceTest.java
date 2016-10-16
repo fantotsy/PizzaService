d206496c30e210c50c6e970098b287c551e82dae
fantotsy <fantotsy@gmail.com>
2016-10-16 16:36:43 +0300

    SimpleAddressTest done.

---
+++ src/test/java/ua/fantotsy/services/address/SimpleAddressServiceTest.java
@@ -0,0 +1,32 @@
+package ua.fantotsy.services.address;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import ua.fantotsy.domain.Address;
+import ua.fantotsy.repository.address.AddressRepository;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleAddressServiceTest {
+    SimpleAddressService addressService;
+    @Mock
+    AddressRepository addressRepositoryMock;
+    @Mock
+    Address addressMock;
+
+    @Before
+    public void setUp() {
+        addressService = spy(new SimpleAddressService(addressRepositoryMock));
+        doReturn(addressMock).when(addressService).createNewAddress();
+    }
+
+    @Test
+    public void testAddAndReturnNewAddressAddsAddress() {
+        addressService.addAndReturnNewAddress("City", "Street");
+        verify(addressRepositoryMock).addNewAddress(addressMock);
+    }
+}
