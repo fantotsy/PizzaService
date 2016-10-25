f069d1647f46a7356407b55260420bf3710a0a2c
Anton_Tsymbal <anton_tsymbal@epam.com>
2016-10-25 13:26:52 +0300

    Hibernate and spring connected.

---
+++ src/main/java/ua/fantotsy/SpringJpaAppRunner.java
@@ -0,0 +1,37 @@
+package ua.fantotsy;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ua.fantotsy.domain.Order;
+import ua.fantotsy.domain.Pizza;
+import ua.fantotsy.services.order.OrderService;
+
+import java.util.Arrays;
+
+public class SpringJpaAppRunner {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext repoContext = new ClassPathXmlApplicationContext("repoContext.xml");
+        System.out.println(Arrays.toString(repoContext.getBeanDefinitionNames()));
+
+        ConfigurableApplicationContext appContext =
+                new ClassPathXmlApplicationContext(new String[]{"appContext.xml"}, repoContext);
+        System.out.println(Arrays.toString(appContext.getBeanDefinitionNames()));
+
+//        OrderService orderService = (OrderService) appContext.getBean("orderService");
+//        orderService.addNewPizza("Diabola", 300.0, Pizza.PizzaTypes.MEAT);
+//        orderService.addNewCustomer("Vasya", "Kyiv", "K18a", true);
+//
+//        Order order1 = orderService.placeNewOrder(orderService.getCustomerById(1), 1, 1, 1, 1, 1);
+//        order1.pay();
+//        Order order2 = orderService.placeNewOrder(orderService.getCustomerById(1), 1, 1);
+//        System.out.println("TOTAL: " + order1.getTotalPrice());
+//        System.out.println("TOTAL: " + order2.getTotalPrice());
+//
+//        System.out.println(order1);
+//        System.out.println(order2);
+
+        repoContext.close();
+        appContext.close();
+    }
+}
