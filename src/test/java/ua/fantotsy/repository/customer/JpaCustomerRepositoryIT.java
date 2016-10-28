4540dca0d6ed82f5ff31e48b75a1539023385dc3
Anton_Tsymbal <anton_tsymbal@epam.com>
2016-10-28 17:31:35 +0300

    Code refactored.

--- src/test/java/ua/fantotsy/repository/customer/JpaCustomerRepositoryIT.java
+++ src/test/java/ua/fantotsy/repository/customer/JpaCustomerRepositoryIT.java
@@ -1,5 +1,7 @@
 package ua.fantotsy.repository.customer;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import ua.fantotsy.domain.AccumulativeCard;
@@ -9,6 +11,7 @@ import ua.fantotsy.repository.RepositoryTestConfig;
 import ua.fantotsy.repository.accumulativeCard.AccumulativeCardRepository;
 import ua.fantotsy.repository.address.AddressRepository;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class JpaCustomerRepositoryIT extends RepositoryTestConfig {
@@ -20,17 +23,46 @@ public class JpaCustomerRepositoryIT extends RepositoryTestConfig {
     @Autowired
     private AccumulativeCardRepository accumulativeCardRepository;
 
+    @Before
+    public void setUp() {
+        Address address = new Address("City1", "Street1");
+        jdbcTemplate.update("INSERT INTO addresses (id, city, street) VALUES (1, ?, ?)",
+                address.getCity(), address.getStreet());
+
+        AccumulativeCard accumulativeCard1 = new AccumulativeCard(12345L);
+        jdbcTemplate.update("INSERT INTO accumulative_cards (id, balance, number) VALUES (1, ?, ?)",
+                accumulativeCard1.getBalance(), accumulativeCard1.getNumber());
+
+        jdbcTemplate.update("INSERT INTO customers (id, name, address_id, accumulative_card_id) VALUES (1, 'Name1', 1, 1)");
+    }
+
+    @After
+    public void tearDown() {
+        jdbcTemplate.update("DELETE FROM customers");
+        jdbcTemplate.update("DELETE FROM accumulative_cards");
+        jdbcTemplate.update("DELETE FROM addresses");
+    }
+
     @Test
     public void testFindCustomerById() {
-
+        Customer customer = customerRepository.findById(1L);
+        long id = customer.getId();
+        assertEquals(1L, id);
     }
 
     @Test
     public void testSaveCustomer() {
-        Address address = addressRepository.save(new Address("City", "Street"));
-        AccumulativeCard accumulativeCard = accumulativeCardRepository.save(new AccumulativeCard(12345L));
-        Customer customer = new Customer("Name", address, accumulativeCard);
+        AccumulativeCard accumulativeCard2 = new AccumulativeCard(12346L);
+        jdbcTemplate.update("INSERT INTO accumulative_cards (id, balance, number) VALUES (2, ?, ?)",
+                accumulativeCard2.getBalance(), accumulativeCard2.getNumber());
+
+        Customer customer = new Customer("Name2", addressRepository.findById(1L), accumulativeCardRepository.findById(2L));
         customer = customerRepository.save(customer);
         assertNotNull(customer.getId());
+
+        String savedCustomer = jdbcTemplate.queryForObject("SELECT name FROM customers WHERE name = ?",
+                    new Object[]{customer.getName()}, String.class);
+        System.out.println(savedCustomer);
+        assertEquals(customer.getName(), savedCustomer);
     }
 }
--- src/test/java/ua/fantotsy/repository/pizza/JpaPizzaRepositoryIT.java
+++ src/test/java/ua/fantotsy/repository/pizza/JpaPizzaRepositoryIT.java
@@ -1,10 +1,13 @@
 package ua.fantotsy.repository.pizza;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import ua.fantotsy.domain.Pizza;
 import ua.fantotsy.repository.RepositoryTestConfig;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class JpaPizzaRepositoryIT extends RepositoryTestConfig {
@@ -12,9 +15,23 @@ public class JpaPizzaRepositoryIT extends RepositoryTestConfig {
     @Autowired
     private PizzaRepository pizzaRepository;
 
+    @Before
+    public void setUp() {
+        Pizza pizza = new Pizza("Name1", 100.0, Pizza.PizzaType.SEA);
+        jdbcTemplate.update("INSERT INTO pizzas (id, name, price, status, type) VALUES (1, ?, ?, ?, ?)",
+                pizza.getName(), pizza.getPrice(), pizza.getStatus().name(), pizza.getType().name());
+    }
+
+    @After
+    public void tearDown(){
+        jdbcTemplate.update("DELETE FROM pizzas");
+    }
+
     @Test
     public void testFindPizzaById() {
-
+        Pizza pizza = pizzaRepository.findById(1L);
+        long id = pizza.getId();
+        assertEquals(1L, id);
     }
 
     @Test
@@ -23,4 +40,5 @@ public class JpaPizzaRepositoryIT extends RepositoryTestConfig {
         pizza = pizzaRepository.save(pizza);
         assertNotNull(pizza.getId());
     }
+
 }
