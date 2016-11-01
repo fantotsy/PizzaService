eb4b0df30ac2e68787d5cae5f161bfd27c747dca
Anton_Tsymbal <anton_tsymbal@epam.com>
2016-11-01 12:56:46 +0200

    Servlet added.

---
+++ src/main/java/ua/fantotsy/web/infrastructure/DispatcherServlet.java
@@ -0,0 +1,32 @@
+package ua.fantotsy.web.infrastructure;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class DispatcherServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (PrintWriter out = resp.getWriter()) {
+            out.println("Hello!");
+        }
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+}
