5cba167d138ea3f07969ec9bde243aa0030be7ac
fantotsy <fantotsy@gmail.com>
2016-11-02 21:28:35 +0200

    SpringMVC updated.

--- src/main/java/ua/fantotsy/web/rest/PizzaRestController.java
+++ src/main/java/ua/fantotsy/web/rest/PizzaRestController.java
@@ -1,16 +1,25 @@
 package ua.fantotsy.web.rest;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ua.fantotsy.domain.Pizza;
+import ua.fantotsy.services.pizza.PizzaService;
 
 @RestController
 public class PizzaRestController {
+    @Autowired
+    private PizzaService pizzaService;
 
-    @RequestMapping(value = "hello", method = RequestMethod.GET)
-    public String hello() {
-        return "<b>Hello from rest controller</b>";
+    @RequestMapping(value = "/pizza/{pizzaId}", method = RequestMethod.GET)
+    public Pizza hello(@PathVariable("pizzaId") Long pizzaId) {
+        return pizzaService.findById(3L);
+    }
+
+    @RequestMapping(
+            value = "/pizza",
+            method = RequestMethod.POST
+    )
+    public void pizza(@RequestBody Pizza pizza) {
+        System.out.println(pizza);
     }
 }
